011c5f5db7619b15cf5a0aeb74f2894f6b992431
kaztro <00229017@uca.edu.sv>
2018-10-24 22:33:07 -0600

    IT'S DONEgit add .

--- js/script.js
+++ js/script.js
@@ -1,56 +1,75 @@
- window.onload = init;
-   
- function init() {
-     let todoList = {
-         listHTML: document.getElementById("todoList"),
-         listTask: [],
-         add(task, priority = false) {
-             let element = document.createElement("li");
-             element.innerText = task;
-             /*element.addEventListener("click", () => {
-                let parent = element.parentNode;
-                if(parent){
-                    parent.removeChild(element);
-                }
-             });*/
-             element.addEventListener("click", function(){
-                console.log(this);
-                let parent = this.parentNode;
-                if(parent){
-                    parent.removeChild(this);
-                }
-             });
-            // Añadir un boton para marcar de finalizado
+window.onload = init;
+
+function init() {
+    let todoList = {
+        listHTML: document.getElementById("todoList"),
+        listTask: [],
+        add(task, priority = false) {
+            let element = document.createElement("li");
+            let eBottom = document.createElement("input");
+            let dBottom = document.createElement("input");
+            let erase = document.createElement("label");
+            let done = document.createElement("label");
+            let theTask = document.createElement("p");
+            theTask.id = task;
+            eBottom.type = "checkbox";
+            dBottom.type = "checkbox";
+            theTask.innerText = task;
+            element.appendChild(theTask);
+            erase.innerText = "Eliminar";
+            done.innerText = "Finalizado";
+            element.appendChild(erase);
+            element.appendChild(eBottom);
+            element.appendChild(done)
+            element.appendChild(dBottom);
+
             // Elmine de la lista
+            eBottom.addEventListener("change", function () {
+                console.log(element);
+                if (this.checked) {
+                    let parent = element.parentNode;
+                    if (parent) parent.removeChild(element);
+                }
+            });
+            
+            // Añadir un boton para marcar de finalizado
+            dBottom.addEventListener("change", function () {
+                if (this.checked) {
+                    document.getElementById(task).style.textDecoration = "line-through";
+                }
+                else {
+                    document.getElementById(task).style.textDecoration = "none";
+                }
+            });
 
-             if (priority) {
-                 this.listTask.unshift({
-                     element,
-                     task
-                 });
-                 this.listHTML.insertBefore(element, this.listHTML.childNodes[0]);
-             } else {
-                 this.listTask.push({
-                     element,
-                     task
-                 });
-                 this.listHTML.appendChild(element);
-             }
-         }
-     }
+            if (priority) {
+                this.listTask.unshift({
+                    element,
+                    task
+                });
+                this.listHTML.insertBefore(element, this.listHTML.childNodes[0]);
+            } else {
+                this.listTask.push({
+                    element,
+                    task
+                });
+                this.listHTML.appendChild(element);
+            }
+        }
+    }
 
-     let form = document.managerTask;
-     form.addEventListener("submit", (evt) => {
-         evt.preventDefault();
-         let task = form.task.value;
+    let form = document.managerTask;
+    form.addEventListener("submit", (evt) => {
+        evt.preventDefault();
+        let task = form.task.value;
 
-         let validTask = /.{2,}/;
-         if (!validTask.test(task)) {
-             console.log("Ingrese una descripcion clara");
-             return false;
-         }
+        let validTask = /.{2,}/;
+        if (!validTask.test(task)) {
+            console.log("Ingrese una descripcion clara");
+            return false;
+        }
 
-         todoList.add(task, form.important.checked);
+        todoList.add(task, form.important.checked);
 
-     });
- }
+    });
+}
